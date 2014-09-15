7f28443158bbf74d461f608299bd453998c12cd6
baseapp <baseapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-15 11:58:15 +0000

    temp update
    git-svn-id: https://plugins.svn.wordpress.org/wpbase-cache/trunk@990116 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpbase-cache.php
+++ wpbase-cache.php
@@ -3,7 +3,7 @@
 Plugin Name: WPBase-Cache
 Plugin URI: https://github.com/baseapp/wpbase-cache
 Description: A wordpress plugin for using all caches on varnish, nginx, php-fpm stack with php-apc. This plugin includes db-cache-reloaded-fix for dbcache.
-Version: 1.0.3
+Version: 1.0.0
 Author: Tarun Bansal
 Author URI: http://blog.wpoven.com
 License: GPL2
