30d968d69ae3fac89f2c0264a8c4135b7fc7fefe
baseapp <baseapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-14 09:09:48 +0000

    headers empty fix
    git-svn-id: https://plugins.svn.wordpress.org/wpbase-cache/trunk@1220735 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wpbase-cache.php
+++ trunk/wpbase-cache.php
@@ -1,27 +1,26 @@
 <?php
 /*
-  Plugin Name: WPBase-Cache
-  Plugin URI: https://github.com/baseapp/wpbase-cache
-  Description: A wordpress plugin for using all caches on varnish, nginx, php-fpm stack with php-apc. This plugin includes db-cache-reloaded-fix for dbcache.
-  Version: 2.1.0
-  Author: Vikrant Datta
-  Author URI: http://blog.wpoven.com
-  License: GPL2
+Plugin Name: WPBase-Cache
+Plugin URI: https://github.com/baseapp/wpbase-cache
+Description: A wordpress plugin for using all caches on varnish, nginx, php-fpm stack with php-apc. This plugin includes db-cache-reloaded-fix for dbcache.
+Version: 2.1.1
+Author: Vikrant Datta
+Author URI: http://blog.wpoven.com
+License: GPL2
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 defined('ABSPATH') or die();
